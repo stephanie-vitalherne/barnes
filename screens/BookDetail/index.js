16e1523b7a7fae5e01cfc493df5f6b73bc0c7e30
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-08-03 16:34:33 -0400

    header done

--- screens/BookDetail/index.js
+++ screens/BookDetail/index.js
@@ -1,13 +1,131 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Image
+} from 'react-native';
 import { COLORS, FONTS, SIZES, icons, images } from '../../constants';
+import { styles } from './styles';
 
-const BookDetail = () => {
+const LineDivider = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.lineDivider}>
+      <View style={styles.line} />
     </View>
   );
+};
+
+const BookDetail = ({ route, navigation }) => {
+  const [book, setBook] = useState(null);
+  useEffect(() => {
+    let { book } = route.params;
+    setBook(book);
+  }, [book]);
+
+  function renderBookInfo() {
+    return (
+      <View style={styles.flex}>
+        <ImageBackground
+          resizeMode="cover"
+          source={book.bookCover}
+          style={styles.infoCover}
+        />
+
+        {/* Color Overlay */}
+        <View
+          style={[styles.infoCover, { backgroundColor: book.backgroundColor }]}
+        />
+        {/* Navigation Header */}
+        <View style={styles.navHeader}>
+          <TouchableOpacity
+            style={styles.nav}
+            onPress={() => navigation.goBack()}>
+            <Image
+              style={[styles.arrow, { tintColor: book.navTintColor }]}
+              source={icons.back_arrow_icon}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+
+          <View style={styles.navTitle}>
+            <Text style={[styles.title, { color: book.navTintColor }]}>
+              Book Detail
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            style={styles.navBtn}
+            onPress={() => console.log('Click More')}>
+            <Image
+              resizeMode="contain"
+              style={[styles.moreBtn, { tintColor: book.navTintColor }]}
+              source={icons.more_icon}
+            />
+          </TouchableOpacity>
+        </View>
+
+        {/* Book Cover */}
+        <View style={styles.bookCoverContainer}>
+          <Image
+            style={styles.bookCover}
+            source={book.bookCover}
+            resizeMode="contain"
+          />
+        </View>
+
+        {/* Name & Author */}
+        <View style={styles.bookDesc}>
+          <Text style={[styles.bookName, { color: book.navTintColor }]}>
+            {book.bookName}
+          </Text>
+          <Text style={[styles.author, { color: book.navTintColor }]}>
+            {book.author}
+          </Text>
+        </View>
+
+        {/* Book Info */}
+        <View style={styles.bookInfo}>
+          {/* Rating */}
+          <View style={styles.bookRating}>
+            <Text style={styles.rating}>{book.rating}</Text>
+            <Text style={styles.ratingTxt}>Rating</Text>
+          </View>
+          <LineDivider />
+
+          {/* Pages */}
+          <View
+            style={[styles.bookRating, { paddingHorizontal: SIZES.radius }]}>
+            <Text style={styles.rating}>{book.pageNo}</Text>
+            <Text style={styles.ratingTxt}>Number of Pages</Text>
+          </View>
+          <LineDivider />
+
+          {/* Language */}
+          <View style={styles.bookRating}>
+            <Text style={styles.rating}>{book.language}</Text>
+            <Text style={styles.ratingTxt}>Language</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  if (book) {
+    return (
+      <View style={styles.mainContainer}>
+        {/* Book Cover */}
+        <View style={styles.coverContainer}>{renderBookInfo()}</View>
+        {/* Description */}
+        <View style={styles.descContainer}></View>
+        {/* Buttons */}
+        <View style={styles.btnContainer}></View>
+      </View>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default BookDetail;
--- screens/BookDetail/styles.js
+++ screens/BookDetail/styles.js
@@ -2,6 +2,108 @@
 import { StyleSheet } from 'react-native';
 import { COLORS, FONTS, SIZES } from '../../constants';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: COLORS.black
+  },
+  coverContainer: {
+    flex: 4
+  },
+  infoCover: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0
+  },
+  navHeader: {
+    flexDirection: 'row',
+    paddingHorizontal: SIZES.radius,
+    height: 80,
+    alignItems: 'flex-end'
+  },
+  nav: {
+    marginLeft: SIZES.base
+  },
+  arrow: {
+    width: 25,
+    height: 25
+  },
+  navTitle: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    ...FONTS.h3
+  },
+  navBtn: {
+    marginRight: SIZES.base
+  },
+  moreBtn: {
+    width: 30,
+    height: 30,
+    alignSelf: 'flex-end'
+  },
+  bookCoverContainer: {
+    flex: 5,
+    paddingTop: SIZES.padding2,
+    alignItems: 'center'
+  },
+  bookCover: {
+    flex: 1,
+    width: 150,
+    height: 'auto'
+  },
+  bookDesc: {
+    flex: 1.8,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  bookName: {
+    ...FONTS.h2
+  },
+  author: {
+    ...FONTS.body3
+  },
+  bookInfo: {
+    flexDirection: 'row',
+    paddingVertical: 20,
+    margin: SIZES.padding,
+    borderRadius: SIZES.radius,
+    backgroundColor: 'rgba(0,0,0,0.3)'
+  },
+  bookRating: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  rating: {
+    color: COLORS.white,
+    ...FONTS.h3
+  },
+  ratingTxt: {
+    color: COLORS.white,
+    ...FONTS.body4
+  },
+  lineDivider: {
+    width: 1,
+    paddingVertical: 5
+  },
+  line: {
+    flex: 1,
+    borderLeftColor: COLORS.lightGray2,
+    borderLeftWidth: 1
+  },
+  descContainer: {
+    flex: 2
+  },
+  btnContainer: {
+    height: 70
+  }
+});
 
 export { styles };
